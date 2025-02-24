19099b122d10fb9fe9d66ff81a66967aaf9d3ecc
shashank1227 <shashanknet50@gmail.com>
2025-02-24 22:55:15 +0530

    Fixed font loading: moved Google Fonts import from styled-components to HTML

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Space+Grotesk:wght@500;700&display=swap');
-
   * {
     margin: 0;
     padding: 0;
